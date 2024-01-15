4d17950b2e285d32b13c2cd9095b327ec9506332
edwinkalinda <edwin.musakalinda@student.vinci.be>
2024-01-15 16:34:00 +0100

    length

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -5,5 +5,13 @@ describe('validations tests suites - isValid', () => {
     test('should return true or false as the function is defined', () => {
         const result = isValid();
         expect(result).toBeDefined();
+        test('should be at least 8 characters long', () => {
+            expect(validGamerTagLength('Game1234')).toBe(true);
+            expect(validGamerTagLength('1234567')).toBe(false);
+         });
+         
     });
 });
+
+
+
