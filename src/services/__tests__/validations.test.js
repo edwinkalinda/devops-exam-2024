f3536ba950a2bbd327d09d3bacd23bfeadd9f37d
edwinkalinda <edwin.musakalinda@student.vinci.be>
2024-01-15 16:36:32 +0100

    length2

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,17 +1,21 @@
 // TODO: Create tests suite for validation function
 const { isValid } = require('../validations');
+const { validateGamerTag } = require('../validations');
 
 describe('validations tests suites - isValid', () => {
     test('should return true or false as the function is defined', () => {
         const result = isValid();
-        expect(result).toBeDefined();
-        test('should be at least 8 characters long', () => {
-            expect(validGamerTagLength('Game1234')).toBe(true);
-            expect(validGamerTagLength('1234567')).toBe(false);
-         });
-         
+        expect(result).toBeDefined();    
     });
 });
+describe('validateGamerTag', () => {
+    test('should be at least 8 characters long', () => {
+      expect(validateGamerTag('Game123!')).toBe(true);
+      expect(validateGamerTag('Gm1!')).toBe(false);
+    });
+});
+
+
 
 
 
